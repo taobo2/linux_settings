a16b5909efb5d35f403fd2f548d291d38ac694cb
Bob Tao <taobo2@163.com>
2016-06-08 14:30:26 +0800

    set accurate colors

--- ps.sh
+++ ps.sh
@@ -2,10 +2,11 @@
 #bgColor=4 #blue
 #bgColor=1 #red
 PS1=$(
-if [ $EUID -ne 0 ]; then bgColor='$(tput setab 4)'; 
+if [ $EUID -ne 0 ]; then bgColor='\[$(tput setab 4)\]'; 
 else bgColor='\[$(tput setab 1)\]';fi; #\[ \] to make command line wrap normally
+
 clearStyle='\[$(tput sgr0)\]'
-echo $bgColor'$(
+echo $bgColor'\[$(tput setaf 7)\[$(
     leftPrompt="$USER @ $HOSTNAME" 
     rightLen=$(($(tput cols) - ${#leftPrompt}))
     printf "%s%*s" "$leftPrompt" $rightLen "$(date +%Y-%m-%d\ %H:%M:%S\ %z)"
@@ -20,7 +21,7 @@ echo $bgColor'$(
     then echo "\w"
     else echo ">\W"
     fi
-    ) \[$(tput setaf 2)\]\$'$clearStyle' '
+    ) \[$(tput setab 0)\]\[$(tput setaf 2)\]\$'$clearStyle' '
     )
 
 
