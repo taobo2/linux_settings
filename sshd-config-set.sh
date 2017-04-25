37e7016d6b4744a5273acb5657472b738ab8be70
Bob Tao <taobo2@163.com>
2017-04-25 16:01:22 +0800

    add forwarding ssh agent

--- sshd-config-set.sh
+++ sshd-config-set.sh
@@ -1,19 +1,23 @@
 #!/bin/bash
 config_file='/etc/ssh/sshd_config'
 backup_file=$config_file'.bak'
+
+if [ -f $backup_file ]; then
+    echo '$backup_file found, recover'
+    mv -f $backup_file $config_file
+else
+    echo '$backup_file not found'
+fi
+
 if [[ $1 == '-u' ]] 
 then
-    if [ -f $backup_file ]; then
-        echo '$backup_file found, recover'
-        mv -f $backup_file $config_file
-    else
-        echo '$backup_file not found, exit'
-    fi
+    exit 0
 else
-    echo 'add settings to configfile'
+    echo 'add settings to $config_file'
     cp -f $config_file $backup_file
     echo 'ClientAliveInterval 120' >> $config_file
     echo 'ClientAliveCountMax 720' >> $config_file
+    echo 'AllowAgentForwarding yes' >> $config_file
 fi
   
 service sshd restart 
