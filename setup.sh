576e780d74d3a3f7f533e97d339a5e34d31508dc
Bob Tao <taobo22@hotmail.com>
2017-09-09 08:40:18 +0800

    add default editor

--- setup.sh
+++ setup.sh
@@ -79,6 +79,7 @@ echo "export HISTFILESIZE=10000" >> $bashrc
 echo 'export HISTIGNORE="rm*:sudo rm*"' >> $bashrc
 echo 'export HISTCONTROL="ignorespace"' >> $bashrc
 echo "export PS1='\[$(tput setaf 3)\]\t \u@\h \W $ \[$(tput sgr0)\]'" >> $bashrc
+echo 'export EDITOR=vim' >> $bashrc
 #echo "source $setDir/ps.sh" >> $bashrc
 echo $endLine >> $bashrc
 checkComResult 'Set .bashrc' $?
